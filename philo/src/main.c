3ffcd1baaeb0685978e0e7418d3a9da8d394ccf5
El Houssain Souhail <sou.ductive@gmail.com>
2025-09-08 15:46:50 +0200

    feat(main): implement program entry point and handle input parsing

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,17 +6,19 @@
 /*   By: esouhail <esouhail@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 08:09:15 by esouhail          #+#    #+#             */
-/*   Updated: 2025/09/03 17:33:06 by esouhail         ###   ########.fr       */
+/*   Updated: 2025/09/03 18:14:00 by esouhail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	t_data	input;
+	t_philosopher philosophers[256];
+	t_data input;
 
 	if (!parse(&input, ac, av) || input.max_meal_count == 0)
 		return (-1 * input.max_meal_count != 0);
+
 	return (0);
 }
