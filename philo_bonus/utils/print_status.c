e664fea2778e1d8fb7c9b5531e2bb24bbe5371bc
El Houssain Souhail <sou.ductive@gmail.com>
2025-10-09 05:53:18 +0200

    Remove unallowed function

--- philo_bonus/utils/print_status.c
+++ philo_bonus/utils/print_status.c
@@ -6,7 +6,7 @@
 /*   By: esouhail <esouhail@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 03:08:01 by esouhail          #+#    #+#             */
-/*   Updated: 2025/10/09 05:50:39 by esouhail         ###   ########.fr       */
+/*   Updated: 2025/10/09 05:52:35 by esouhail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,5 @@ void	print_status(t_philo *philo, char *status)
 	sem_wait(philo->table->write);
 	timestamp = get_time_in_ms() - philo->table->simulation_start_time;
 	printf("%d %d %s\n", timestamp, philo->id, status);
-	fflush(stdout);
 	sem_post(philo->table->write);
 }
